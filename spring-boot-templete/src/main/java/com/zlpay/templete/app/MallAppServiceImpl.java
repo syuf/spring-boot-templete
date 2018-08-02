76e0c4acd7058d2016ce249917e7539cba25ab05
shiyuanfei <356418670@qq.com>
2018-08-02 10:47:45 +0800

    新增hessian实现

---
+++ spring-boot-templete/src/main/java/com/zlpay/templete/app/MallAppServiceImpl.java
@@ -0,0 +1,27 @@
+/**   
+ * Copyright © 2018 zlpay.
+ */
+package com.zlpay.templete.app;
+
+import org.springframework.stereotype.Service;
+
+import com.zlpay.common.app.mall.MallAppService;
+
+import lombok.extern.slf4j.Slf4j;
+
+/** 
+ * @Description: TODO
+ * @author: syuf
+ * @date: 2018年8月2日 上午10:42:13  
+ */
+@Slf4j
+@Service("mallAppService")
+public class MallAppServiceImpl implements MallAppService {
+
+	@Override
+	public String trade(String merchNo, String txCode, String data) {
+		log.info("收到请求{}",merchNo);
+		return "hello " + merchNo;
+	}
+
+}
--- spring-boot-templete/src/main/java/com/zlpay/templete/configuration/HessianConfiguration.java
+++ spring-boot-templete/src/main/java/com/zlpay/templete/configuration/HessianConfiguration.java
@@ -1,13 +1,19 @@
 package com.zlpay.templete.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.caucho.HessianServiceExporter;
+
+import com.zlpay.common.app.mall.MallAppService;
+
 public class HessianConfiguration {
 	
+	private MallAppService mallAppService;
 	
-//	@Bean
-//	public HessianProxyFactoryBean initHessianConfiguration(HessianProperties properties){
-//		HessianProxyFactoryBean hessianFactory = new HessianProxyFactoryBean();
-//		hessianFactory.setServiceUrl(properties.getActivemqUrl());
-//		hessianFactory.setServiceInterface(ActivemqAppService.class);
-//		return hessianFactory;
-//	}
+	@Bean(name = "/mallAppService")
+    public HessianServiceExporter accountService() {
+        HessianServiceExporter exporter = new HessianServiceExporter();
+        exporter.setService(mallAppService);
+        exporter.setServiceInterface(MallAppService.class);
+        return exporter;
+    }
 }
